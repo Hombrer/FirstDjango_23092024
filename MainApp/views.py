59c73919d55cb47053f021334f1a1a1eba84a2a7
Hombrer <5208643@mail.ru>
2024-09-24 16:22:06 +0300

    Done. Practice M2:task1 and task5

--- MainApp/views.py
+++ MainApp/views.py
@@ -2,14 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
-author = {
-    "Имя": "Иван",
-    "Отчество": "Петрович",
-    "Фамилия": "Иванов",
-    "телефон": "8-923-600-01-02",
-    "email": "vasya@mail.ru"
 
-}
 
 items = [
    {"id": 1, "name": "Кроссовки abibas" ,"quantity":5},
@@ -29,38 +22,34 @@ def home(request):
 
 
 def about(request):
+    author = {
+        "name": "Иван",
+        "middle_name": "Петрович",
+        "last_name": "Иванов",
+        "phone": "8-923-600-01-02",
+        "email": "vasya@mail.ru"
+    }
     text = f"""
-    <h2> Информация об авторе </h2>
-    Имя: <b>{author['Имя']}</b><br>
-    Отчество: <b>{author['Отчество']}</b><br>
-    Фамилия: <b>{author['Фамилия']}</b><br>
-    Телефон: <b>{author['телефон']}</b><br>
-    Email: <b>{author['email']}</b><br>
+        <h2> Информация об авторе </h2>
+        Имя: <b>{author['name']}</b><br>
+        Отчество: <b>{author['middle_name']}</b><br>
+        Фамилия: <b>{author['last_name']}</b><br>
+        Телефон: <b>{author['phone']}</b><br>
+        Email: <b>{author['email']}</b><br>
     """
     return HttpResponse(text)
 
 
-# /item/1
-# /item/2
-# ....
-# /item/n-1
-# /item/n
 def get_item(request, item_id: int):
     """ Функция по item_id нужного элемента вернет имя и кол-во. """
     for item in items:
         if item["id"] == item_id:
-            result = f"""
-            <h2> Имя: {item['name']} </h2>
-            <p>Количество: {item['quantity']}</p>
-            <p><a href="/items"> Назад к списку товаров </a></p>
-            """
-            return HttpResponse(result)
+            context = {"item": item}
+            return render(request, "item_page.html", context)
     # Если элемент не найден - нужно вернуть соотвествующий ответ(response)
     return HttpResponseNotFound(f"Item with id={item_id} not found.")
 
 
 def get_items(request):
-    context = {
-        "items": items
-    }
+    context = {"items": items}
     return render(request, "items_list.html", context)
