a00326afd83410cb21ad64445f196c69c975c2b4
Hombrer <5208643@mail.ru>
2024-09-25 14:39:27 +0300

    Done. Practice M3:part2, task1-3

--- MainApp/views.py
+++ MainApp/views.py
@@ -6,16 +6,6 @@ from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
 
-
-# items = [
-#    {"id": 1, "name": "Кроссовки abibas" ,"quantity":5},
-#    {"id": 2, "name": "Куртка кожаная" ,"quantity":2},
-#    {"id": 5, "name": "Coca-cola 1 литр" ,"quantity":12},
-#    {"id": 7, "name": "Картофель фри" ,"quantity":0},
-#    {"id": 8, "name": "Кепка" ,"quantity":124},
-# ]
-
-
 def home(request):
     context = {
         "name": "Петров Иван Николаевич",
--- README.md
+++ README.md
@@ -19,9 +19,13 @@ python manage.py shell_plus --ipython
 ```
 
 ## Выгрузка и загрузка данных
-### Выгрузить данные из БД
+### Выгрузить данные из БД (Linux)
 ```
 python manage.py dumpdata MainApp --indent 4 > MainApp/fixtures/save_all.json
+```
+### Выгрузить данные из БД (Windows)
+```
+python -Xutf8 manage.py dumpdata MainApp --indent 4 -o MainApp/fixtures/save_all.json
 ```
 
 ###  Загрузить данные в БД
