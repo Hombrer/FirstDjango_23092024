77ebdb5f912eaf8fc72dc264e10e7e38fa81df7d
Hombrer <5208643@mail.ru>
2024-09-25 12:29:07 +0300

    Done. Practice M3:task2 and add author template

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
+from MainApp.models import Item
+# Нужный тип исключения для функции get_item()
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
 
@@ -29,18 +32,7 @@ def about(request):
         "phone": "8-923-600-01-02",
         "email": "vasya@mail.ru"
     }
-    text = f"""
-        <header>
-        / <a href="/"> Home </a> / <a href="/items"> Items</a> / <a href="/about"> About </a>
-        </header>
-        <h2> Информация об авторе </h2>
-        Имя: <b>{author['name']}</b><br>
-        Отчество: <b>{author['middle_name']}</b><br>
-        Фамилия: <b>{author['last_name']}</b><br>
-        Телефон: <b>{author['phone']}</b><br>
-        Email: <b>{author['email']}</b><br>
-    """
-    return HttpResponse(text)
+    return render(request, 'about.html', {'author': author})
 
 
 def get_item(request, item_id: int):
@@ -54,5 +46,6 @@ def get_item(request, item_id: int):
 
 
 def get_items(request):
+    items = Item.objects.all()
     context = {"items": items}
     return render(request, "items_list.html", context)
